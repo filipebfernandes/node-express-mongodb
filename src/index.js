018b1b26452edd825cdfbd158b598a7b1ce27d57
filipebfernandes <f92fernandes@gmail.com>
2019-07-30 15:21:02 +0100

    add express first route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import "dotenv/config";
-
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
-app.listen(3000, () => console.log("App listening on port 3000!"));
+app.use(cors());
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(process.env.PORT, () =>
+  console.log(`App listening on port ${process.env.PORT}!`)
+);
 
 console.log(process.env.MY_SECRET);
